1c8e34e2e4da46cfca7e14474f3feea5f8cb1ea7
will <cg01will@gmailcom>
2018-05-18 12:51:07 +0200

    refactored code, split up in function

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,14 +4,23 @@
   let paragraphs = document.getElementsByTagName("p");
   let images = document.getElementsByTagName("img");
 
-  for (var i = 0; i < paragraphs.length; i++) {
-    console.log(paragraphs[i].innerHTML);
-    paragraphs[i].innerHTML = "whoop whoop";
-  }
+  replaceAllElementsInnerHTML("p","test it");
+  replaceAllElementsSrc("img", "http://www.catster.com/wp-content/uploads/2017/12/A-kitten-meowing.jpg");
 
-  for (var i = 0; i < images.length; i++) {
-    console.log(images[i].src);
-    console.log(images[i].alt);
-    images[i].src = "";
-  }
 })();
+
+function replaceAllElementsInnerHTML(tagName, newInnerHTML){
+  let elements = document.getElementsByTagName(tagName);
+
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].innerHTML = newInnerHTML;
+  }
+}
+
+function replaceAllElementsSrc(tagName, newSrc){
+  let elements = document.getElementsByTagName(tagName);
+
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].src = newSrc;
+  }
+}
