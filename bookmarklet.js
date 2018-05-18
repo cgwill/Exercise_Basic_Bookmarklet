f18c1c6a329a2acd8f436687d0dfd7744b20b976
will <cg01will@gmailcom>
2018-05-18 11:44:43 +0200

    img src replacement added

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,9 +2,15 @@
 (function() {
   console.log("bookmarklet started");
   let paragraphs = document.getElementsByTagName("p");
+  let images = document.getElementsByTagName("img");
 
   for (var i = 0; i < paragraphs.length; i++) {
     console.log(paragraphs[i].innerHTML);
-    paragraphs[i].innerHTML = "whoop whoop"
+    paragraphs[i].innerHTML = "whoop whoop";
+  }
+
+  for (var i = 0; i < images.length; i++) {
+    console.log(images[i].src);
+    images[i].src = "";
   }
 })();
